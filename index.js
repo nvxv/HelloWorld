81f82535dd1cdff0a4348c21d3f8d0d75eaae513
gioalers <101071287+gioalers@users.noreply.github.com>
2023-04-26 14:45:25 -0400

    yes

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+window.onload = init;
+function init() {
+const helloBtn = document.getElementById("helloBtn");
+helloBtn.onclick = onHelloBtnClicked;
+}
+function onHelloBtnClicked() {
+alert("Hi there!");
+}
